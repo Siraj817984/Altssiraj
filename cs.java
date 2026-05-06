fb3fa363f63ab35d257909c8af108c1a8dff44c1
Shaik Siraj <msirajshaik192@gmail.com>
2026-05-06 10:34:17 +0530

    Add files via upload
    my java program

---
+++ cs.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class cs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if( x % 2 == 0){
+            System.out.println("Even");
+        }else{
+            System.out.println("Odd");
+            
+        
+    }
+    
+    }
+}
+// {
+//     if 
+//     else 
+//     else if
+// }
---
+++ loops.java
@@ -0,0 +1,27 @@
+
+
+
+
+
+public class loops {
+    public static void main(String[] args) {
+        // for(int counter = 0; counter < 10;  counter+1){
+        //     System.out.println("Assalaimu Walaikum Siraj Sahab");
+        // }
+       
+        /* 
+        //---->WHILE LOOP
+
+         while(Condition){
+            do something
+         }
+        
+        ==> EXAMPLE
+        int i=0;
+        while (i<11) {
+            
+          System.out.println(i);  
+            i++;
+        }*/          
+    }
+}
